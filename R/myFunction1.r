2315bbfd2b1b1181c0f24968e97290755c10c799
AndresMCB <cifam001@mymail.unisa.edu.au>
2022-11-11 22:05:14 +1030

    commit 2

--- R/myFunction1.r
+++ R/myFunction1.r
@@ -1,3 +1,16 @@
-myFunction1 <- function(){
+#' function to display a message
+#'
+#' @param x a parameter to test whether the function receives a value
+#' @examples
+#' myFunction("test paremeter")
+#'
+
+
+myFunction1 <- function(x=NULL){
 message("Test function to shown how to create scripts inside a package")
+if(!is.null(x)){
+  message("you have also passed the following parameter")
+  print (x)
 }
+
+  }
