9ba7b77ca06aa7dce9111ba77ea2da878dcb585a
AndresMCB <cifam001@mymail.unisa.edu.au>
2022-11-18 23:01:33 +1030

    function one.holdout.cv created

--- R/one.holdout.cv.r
+++ R/one.holdout.cv.r
@@ -1,25 +1,25 @@
 ##' Funcion que recibe un dataset y retorna los sub datasets correspondientes
 ##' a entrenamiento (training) y pruebas (testing)
-##' 
+##'
 ##' @title one.holdout.cv
-##' @param dataset matriz de datos que contiene muestras en cada una 
+##' @param dataset matriz de datos que contiene muestras en cada una
 ##'   de las filas y variables en cada una de las columnas
 ##' @param percentTrain numero decimal que corresponde al porcentaje
 ##'   de muestras deseadas para entrenamiento.
-##' 
+##'
 ##' @return una lista con dos matrices. training, la matriz de entrenamiento
-##'    y testing, la matris de prueba.
-##' 
+##'    y testing, la matriz de prueba.
+##'
 ##' @author Andres Mauricio Cifuentes Bernal
 ##'
 ##' @references
-##' 
 ##'
-##' @seealso 
+##'
+##' @seealso
 ##'
 ##' @examples
 ##'
-##' library(tidiverse)
+##' library(tidyverse)
 ##' data <- one.holdout.cv(dataset = iris
 ##'                        , percentTrain = 0.75)
 ##' View(data$training)
@@ -27,14 +27,14 @@
 
 one.holdout.cv <- function(dataset
                            , percentTrain = 0.7){
-  index <- sample(1:nrow(dataset) 
-                  ,percentTrain*nrow(dataset) 
+  index <- sample(1:nrow(dataset)
+                  ,percentTrain*nrow(dataset)
                   ,replace=F)
-  
+
   training <- dataset[index,,drop=FALSE]
   testing <-dataset[-index,,drop=FALSE]
-  
+
   return(list(training = training
               ,testing = testing ))
-  
+
 }
