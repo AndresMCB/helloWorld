7703b738f1824e3e4bc467a28544c54754703a1b
AndresMCB <cifam001@mymail.unisa.edu.au>
2022-11-19 10:11:22 +1030

    Update of one.holdout.cv function

--- R/one.holdout.cv.r
+++ R/one.holdout.cv.r
@@ -1,21 +1,17 @@
-##' Funcion que recibe un dataset y retorna los sub datasets correspondientes
-##' a entrenamiento (training) y pruebas (testing)
+##' Funcion que recibe un dataset y retorna los sub datasets
+##' correspondientes a entrenamiento (training) y pruebas (testing)
 ##'
 ##' @title one.holdout.cv
-##' @param dataset matriz de datos que contiene muestras en cada una
-##'   de las filas y variables en cada una de las columnas
-##' @param percentTrain numero decimal que corresponde al porcentaje
+##'
+##' @param dataset Matriz de datos con n muestras (filas)
+##' y m variables (columnas)
+##'
+##' @param percentTrain Numero decimal que corresponde al porcentaje
 ##'   de muestras deseadas para entrenamiento.
 ##'
-##' @return una lista con dos matrices. training, la matriz de entrenamiento
+##' @return Una lista con dos matrices. training, la matriz de entrenamiento
 ##'    y testing, la matriz de prueba.
-##'
 ##' @author Andres Mauricio Cifuentes Bernal
-##'
-##' @references
-##'
-##'
-##' @seealso
 ##'
 ##' @examples
 ##'
@@ -25,16 +21,12 @@
 ##' View(data$training)
 ##' View(data$testing)
 
-one.holdout.cv <- function(dataset
-                           , percentTrain = 0.7){
-  index <- sample(1:nrow(dataset)
-                  ,percentTrain*nrow(dataset)
-                  ,replace=F)
+one.holdout.cv <- function(dataset, percentTrain =  0.7){
+  index <- sample(1:nrow(dataset), percentTrain *nrow(dataset)
+                  ,replace = FALSE)
 
-  training <- dataset[index,,drop=FALSE]
-  testing <-dataset[-index,,drop=FALSE]
+  training <- dataset[index,,drop=F]
+  testing <- dataset[-index,, drop=F]
 
-  return(list(training = training
-              ,testing = testing ))
-
+  return(list(training = training, testing = testing))
 }
